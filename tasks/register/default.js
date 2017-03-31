3651efd58c50ad9fa1bbc4ae61a452ed1059d148
Mike McNeil <mikermcneil@users.noreply.github.com>
2017-03-31 01:38:41 -0500

    Update boilerplate default grunt task to fix indentation (and so this example matches the latest from sails-generate.)

--- tasks/register/default.js
+++ tasks/register/default.js
@@ -14,12 +14,12 @@
  */
 module.exports = function (grunt) {
 
-  
-    grunt.registerTask('default', [
-      'compileAssets',
-      'linkAssets',
-      'watch'
-    ]);
-  
+
+  grunt.registerTask('default', [
+    'compileAssets',
+    'linkAssets',
+    'watch'
+  ]);
+
 
 };
