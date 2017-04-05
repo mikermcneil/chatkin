6e8276b8f15539e2ff19d8d2ce6bb920cb394365
Rachael Shaw <r@rachael.wtf>
2017-04-05 13:27:24 -0500

    Take evict-inactive-users back out of timeout.

--- scripts/evict-inactive-users.js
+++ scripts/evict-inactive-users.js
@@ -12,41 +12,39 @@ require('machine-as-script')({
 
   fn: function(inputs, exits) {
 
-    setTimeout(function() {
-      // Grab a list of inactive users with non-null zones.
-      User.find({
-        // TODO: add constraint so that this only applies to inactive users
-        // '!=': null // TODO: put this back once it works
+    // Grab a list of inactive users with non-null zones.
+    User.find({
+      // TODO: add constraint so that this only applies to inactive users
+      // '!=': null // TODO: put this back once it works
+    })
+    .exec(function(err, inactiveUsers){
+      if(err) { return exits.error(err); }
+
+      User.update({
+        // todo
       })
-      .exec(function(err, inactiveUsers){
+      .set({
+        currentZone: null
+      })
+      .exec(function(err){
         if(err) { return exits.error(err); }
 
-        User.update({
-          // todo
-        })
-        .set({
-          currentZone: null
-        })
-        .exec(function(err){
-          if(err) { return exits.error(err); }
+        _.each(inactiveUsers, function(inactiveUser) {
+          console.log('inactive user was in zone: '+inactiveUser.currentZone);
+          if(_.isNull(inactiveUser.currentZone)) { return; }
 
-          _.each(inactiveUsers, function(inactiveUser) {
-            console.log('inactive user was in zone: '+inactiveUser.currentZone);
-            if(_.isNull(inactiveUser.currentZone)) { return; }
-
-            Zone.publish([inactiveUser.currentZone], {
-              verb: 'userLeft',
-              username: inactiveUser.username
-            });
+          Zone.publish([inactiveUser.currentZone], {
+            verb: 'userLeft',
+            username: inactiveUser.username
           });
+        });
 
-          sails.log('Finished evicting %d inactive users.', inactiveUsers.length);
+        sails.log('Finished evicting %d inactive users.', inactiveUsers.length);
 
-          return exits.success();
+        return exits.success();
 
-        }, exits.error);//</ User.update().exec() >
-      }, exits.error);//</ User.find().exec() >
-    },5000);
+      }, exits.error);//</ User.update().exec() >
+    }, exits.error);//</ User.find().exec() >
 
   }
 
