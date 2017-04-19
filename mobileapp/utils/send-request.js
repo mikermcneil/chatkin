f8abb6a61de86996c9a9297c38dc90beff3f9e21
Mike McNeil <michael.r.mcneil@gmail.com>
2017-04-19 04:31:26 -0500

    Deduplicate some logic

--- mobileapp/utils/send-request.js
+++ mobileapp/utils/send-request.js
@@ -40,65 +40,67 @@ module.exports = function sendRequest(options, done){
   }
 
   if (!_.isFunction(done)) { throw new Error('Valid node-style callback function must be provided as the 2nd argument!'); }
-  if (!_.isObject(options)) { return done(new Error('`options` dictionary should be provided as the 1st argument!')); }
 
 
-  if (_.isUndefined(options.baseUrl)) {
-    options.baseUrl = DEFAULT_API_SERVER_BASE_URL;
-  }
-  if (_.isUndefined(options.headers)) {
-    options.headers = {};
-  }
+  (function (proceed){
 
-  if (_.isUndefined(options.url)) { return done(new Error('`url` is required')); }
-  if (_.isUndefined(options.method)) { return done(new Error('`method` is required')); }
-
-  // Prepare body (if relevant)
-  //  • if no body, this is just `undefined`
-  //  • if body is string primitive, it does not get double-wrapped (e.g. 'foo' stays 'foo', not '"foo"')
-  var serializedBodyMaybe;
-  var didJSONStringify;
-  if (_.isUndefined(options.body)) {
-    serializedBodyMaybe = undefined;
-    didJSONStringify = false;
-  }
-  else if (_.isString(options.body)) {
-    serializedBodyMaybe = options.body;
-    didJSONStringify = false;
-  }
-  else {
-    serializedBodyMaybe = JSON.stringify(options.body);
-    didJSONStringify = true;
-  }
-
-  // Add JSON content-type header if the body was stringified.
-  if (didJSONStringify) {
-    options.headers['Content-Type'] = 'application/json';
-  }
+    if (!_.isObject(options)) { return proceed(new Error('`options` dictionary should be provided as the 1st argument!')); }
 
 
-  fetch(options.baseUrl + options.url, {
-    method: options.method,
-    headers: options.headers,
-    body: serializedBodyMaybe
-  })
-  .then(function (res) {
+    if (_.isUndefined(options.baseUrl)) {
+      options.baseUrl = DEFAULT_API_SERVER_BASE_URL;
+    }
+    if (_.isUndefined(options.headers)) {
+      options.headers = {};
+    }
 
-    res.text()
-    .then(function (plainText) {
+    if (_.isUndefined(options.url)) { return proceed(new Error('`url` is required')); }
+    if (_.isUndefined(options.method)) { return proceed(new Error('`method` is required')); }
 
-      var resInfo = {
-        statusCode: +res.status,
-        headers: res.headers,
-        body: plainText
-      };
+    // Prepare body (if relevant)
+    //  • if no body, this is just `undefined`
+    //  • if body is string primitive, it does not get double-wrapped (e.g. 'foo' stays 'foo', not '"foo"')
+    var serializedBodyMaybe;
+    var didJSONStringify;
+    if (_.isUndefined(options.body)) {
+      serializedBodyMaybe = undefined;
+      didJSONStringify = false;
+    }
+    else if (_.isString(options.body)) {
+      serializedBodyMaybe = options.body;
+      didJSONStringify = false;
+    }
+    else {
+      serializedBodyMaybe = JSON.stringify(options.body);
+      didJSONStringify = true;
+    }
 
-      // We'll treat a non-2xx status code as an error, but we'll
-      // give it a special error code so it's easily digested by
-      // our friends up in userland.
-      if(resInfo.statusCode >= 300 || resInfo.statusCode < 200) {
-        try {
-          return done(flaverr({
+    // Add JSON content-type header if the body was stringified.
+    if (didJSONStringify) {
+      options.headers['Content-Type'] = 'application/json';
+    }
+
+    fetch(options.baseUrl + options.url, {
+      method: options.method,
+      headers: options.headers,
+      body: serializedBodyMaybe
+    })
+    .then(function (res) {
+
+      res.text()
+      .then(function (plainText) {
+
+        var resInfo = {
+          statusCode: +res.status,
+          headers: res.headers,
+          body: plainText
+        };
+
+        // We'll treat a non-2xx status code as an error, but we'll
+        // give it a special error code so it's easily digested by
+        // our friends up in userland.
+        if(resInfo.statusCode >= 300 || resInfo.statusCode < 200) {
+          return proceed(flaverr({
             code: 'E_NON_200_RESPONSE',
             body: resInfo.body,
             statusCode: resInfo.statusCode,
@@ -106,85 +108,65 @@ module.exports = function sendRequest(options, done){
           }, new Error(
             'Server responded with '+resInfo.statusCode+(_.isUndefined(resInfo.body) ? '.' : ': '+resInfo.body)
           )));
-        } catch (e) {
-          console.warn('Unhandled error was thrown in an asynchronous callback! (see error log)');
-          console.error(e);
-          return;
-        }
-      }//-•
+        }//-•
 
-      // Otherwise we'll consider it a success.
+        // Otherwise we'll consider it a success.
 
-      // If there was no body, we're done.
-      if (_.isUndefined(resInfo.body)) {
+        // If there was no body, we're proceed.
+        if (_.isUndefined(resInfo.body)) {
+          return proceed(undefined, resInfo);
+        }//-•
+
+
+        // But otherwise, we'll attempt to parse the raw response body as JSON.
+
+        if (!_.isString(resInfo.body)) { return proceed(new Error('Consistency violation: Something fishy is going on.  If present, the raw response body should be a string at this point. But instead got: '+resInfo.body)); }
+
+        var parsedResponseBody;
         try {
-          return done(undefined, resInfo);
+          parsedResponseBody = JSON.parse(resInfo.body);
         } catch (e) {
-          console.warn('Unhandled error was thrown in an asynchronous callback! (see error log)');
-          console.error(e);
-          return;
+          // Note that this approach CAN NEVER correctly understand the situation where the server
+          // sends back a string like `"foo"` in plain text.  It will always think it is `foo`, because
+          // it will successfully JSON parse it.  Fortunately, this case almost never comes up.
+          parsedResponseBody = resInfo.body;
         }
-      }//-•
 
+        resInfo.data = parsedResponseBody;
 
-      // But otherwise, we'll attempt to parse the raw response body as JSON.
+        return proceed(undefined, resInfo);
 
-      if (!_.isString(resInfo.body)) { return done(new Error('Consistency violation: Something fishy is going on.  If present, the raw response body should be a string at this point. But instead got: '+resInfo.body)); }
+      }).catch(proceed);//</ .text() >
 
-      var parsedResponseBody;
-      try {
-        parsedResponseBody = JSON.parse(resInfo.body);
-      } catch (e) {
-        // Note that this approach CAN NEVER correctly understand the situation where the server
-        // sends back a string like `"foo"` in plain text.  It will always think it is `foo`, because
-        // it will successfully JSON parse it.  Fortunately, this case almost never comes up.
-        parsedResponseBody = resInfo.body;
-      }
-
-      resInfo.data = parsedResponseBody;
-
-      try {
-        return done(undefined, resInfo);
-      } catch (e) {
-        console.warn('Unhandled error was thrown in an asynchronous callback! (see error log)');
-        console.error(e);
-        return;
-      }
-
-    })
+    })//</ fetch()...then() >
     .catch(function(err){
-      try {
-        return done(err);
-      } catch (e) {
-        console.warn('Unhandled error was thrown in an asynchronous callback! (see error log)');
-        console.error(e);
-        return;
-      }
-    });
 
-  })//</then>
-  .catch(function(err){
+      // Negotiate network / "request failed" error:
+      var isRequestFailedError = (err.name === 'TypeError');
+      // ```
+      // > "A fetch() promise rejects with a TypeError when a network error is encountered"
+      // > (https://developer.mozilla.org/en-US/docs/Web/API/WindowOrWorkerGlobalScope/fetch)
+      // ```
 
-    // Negotiate network / "request failed" error:
-    var isRequestFailedError = (err.name === 'TypeError');
-    // ```
-    // > "A fetch() promise rejects with a TypeError when a network error is encountered"
-    // > (https://developer.mozilla.org/en-US/docs/Web/API/WindowOrWorkerGlobalScope/fetch)
-    // ```
-
-    try {
-      if (!isRequestFailedError) { return done(err); }
+      if (!isRequestFailedError) { return proceed(err); }
       else {
-        return done(flaverr({code: 'E_OFFLINE'}, new Error(
+        return proceed(flaverr({code: 'E_OFFLINE'}, new Error(
           'Cannot communicate with server.  Are you sure you\'re connected to the internet?  '+
           'If you\'re sure you are, then there is probably a proxy issue, or our server is down.'
         )));
       }
+    });//</ fetch()...catch() >
+
+  })(function (err, resInfo){
+
+    try {
+      return done(err, resInfo);
     } catch (e) {
       console.warn('Unhandled error was thrown in an asynchronous callback! (see error log)');
       console.error(e);
       return;
     }
-  });
+
+  });//</ self-calling function >
 
 };
