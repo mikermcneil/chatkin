2abb5497e978a93b7726e18d9ed4a601438386b3
Mike McNeil <michael.r.mcneil@gmail.com>
2017-10-01 22:39:54 -0500

    Add Province model (just a test to help diagnose and fix https://trello.com/c/jsGY4320)

---
+++ api/models/Province.js
@@ -0,0 +1,20 @@
+/**
+ * Province.js
+ *
+ * @description :: A model definition.  Represents a database table/collection/etc.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  // TODO: remove this-- it's just a test re https://github.com/balderdashy/waterline/pull/1519
+  attributes: {
+
+    zones: {
+      collection: 'Zone'
+    }
+
+  },
+
+};
+
